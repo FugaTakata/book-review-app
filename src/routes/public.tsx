a6912ee7df9e8dc811e34264d5d0271ba273e790
FugaTakata <maple1213goo@gmail.com>
2022-05-08 19:29:37 +0900

    add routes

---
+++ src/routes/public.tsx
@@ -0,0 +1,10 @@
+import { RouteObject } from "react-router-dom";
+
+import { AuthRoutes } from "features/auth/routes";
+
+export const publicRoutes: RouteObject[] = [
+  {
+    path: "/auth/*",
+    element: <AuthRoutes />,
+  },
+];
