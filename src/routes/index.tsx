6dc79b99c3e3fc4f03f4b153402466068c0faa92
FugaTakata <maple1213goo@gmail.com>
2022-05-09 20:50:55 +0900

    add bg

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { useRoutes } from "react-router-dom";
 
+import { Box } from "@chakra-ui/react";
 import { publicRoutes } from "routes/public";
 
 export const AppRoutes = () => {
   const element = useRoutes(publicRoutes);
 
-  return <>{element}</>;
+  return <Box bg={"gray.50"}>{element}</Box>;
 };
